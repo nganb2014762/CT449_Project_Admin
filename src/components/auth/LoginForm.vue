3e6328028aa2784fba9e09af964612772a433925
nganb2014762 <nganb2014762@student.ctu.edu.vn>
2024-04-16 17:23:25 +0700

    fix

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -12,9 +12,9 @@
             <Field name="password" type="password" class="form-control" v-model="loginData.password" />
             <ErrorMessage name="password" class="error-feedback text-danger" />
         </div>
-        <div class="text-center">
+        <!-- <div class="text-center">
             <p>Bạn chưa có tài khoản? <router-link to="/register">Đăng ký</router-link></p>
-        </div>
+        </div> -->
         <div class="form-group d-flex justify-content-center">
             <button class="btn btn-primary w-100" type="submit">Đăng nhập
             </button>
